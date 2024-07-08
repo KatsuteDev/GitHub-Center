1bff2d075660656d97234bccf19e46ae48950e28
Katsute <58778985+Katsute@users.noreply.github.com>
2024-07-07 22:19:52 -0400

    simplify tests

--- test.js
+++ test.js
@@ -1,37 +1,25 @@
-const assert = require("assert");
 const puppeteer = require("puppeteer");
+
+const URL = "https://github.com/KatsuteDev/GitHub-Center";
+
+const selectors = [
+    `.AppHeader > div`,
+    `#repository-container-header`,
+    `body:not(.project-page, .full-width) #repo-content-turbo-frame > div:not(:has(> react-app[app-name="react-code-view"]))`,
+    `#repository-container-header > .container-xl`,
+    `#repo-content-turbo-frame .container-xl`,
+    `#repository-container-header > .container-xl > .border-bottom`,
+];
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto("https://github.com/KatsuteDev/GitHub-Center");
-
-    const selectorExists = async (selector) => {
-        try{
-            await page.waitForSelector(selector, { timeout: 5000 });
-            return true;
-        }catch(e){
-            return false;
-        }
-    };
-
-    // tests
-
-    const tests = [
-        `.AppHeader > div`,
-        `#repository-container-header`,
-        `body:not(.project-page, .full-width) #repo-content-turbo-frame > div:not(:has(> react-app[app-name="react-code-view"]))`,
-        `#repository-container-header > .container-xl`,
-        `#repo-content-turbo-frame .container-xl`,
-        `#repository-container-header > .container-xl > .border-bottom`,
-    ];
-
-    // test run
+    await page.goto(URL);
 
     let failed = false;
-    for(const s of tests){
+    for(const s of selectors){
         try{
-            assert(await selectorExists(s));
+            await page.waitForSelector(s, { timeout: 5000 });
             console.info('✅', s);
         }catch(e){
             console.error('❌', s);
