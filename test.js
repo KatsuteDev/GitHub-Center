004ecd89c9935ec2d03fd9c11047704f09604eb6
Katsute <58778985+Katsute@users.noreply.github.com>
2024-07-14 16:47:21 -0400

    Set maximum width on more pages (#15)

--- test.js
+++ test.js
@@ -4,11 +4,16 @@ const URL = "https://github.com/KatsuteDev/GitHub-Center";
 
 const selectors = [
     // `.AppHeader > div`, // requires login
+    `body:not(.project-page) .application-main:not(:has(> div > projects-v2, > div > .feed-background))`,
     `#repository-container-header`,
-    `body:not(.project-page, .full-width) #repo-content-turbo-frame > div:not(:has(> react-app[app-name="react-code-view"]))`,
-    // `#repository-container-header > .container-xl`, // requires login
-    `#repo-content-turbo-frame .container-xl`,
-    // `#repository-container-header > .container-xl > .border-bottom`, // requires login
+    `#js-repo-pjax-container:not(:has(> projects-v2))`,
+    `body:not(.project-page) #repo-content-turbo-frame > div`,
+
+    `#repository-container-header > div`,
+    `#repo-content-pjax-container > div`,
+    // `body:not(.project-page) #repo-content-turbo-frame > div:not(#repo-content-pjax-container)`, // uses turbo test case
+
+    // `#repository-container-header > div:last-child > div:last-child` // requires login
 ];
 
 (async () => {
@@ -19,6 +24,18 @@ const selectors = [
     let failed = false;
     for(const s of selectors){
         try{
+            await page.waitForSelector(s, { timeout: 5000 });
+            console.info('✅', s);
+        }catch(e){
+            console.error('❌', s);
+            failed = true;
+        }
+    }
+
+    {
+        const s = `body:not(.project-page) #repo-content-turbo-frame > div:not(#repo-content-pjax-container)`;
+        try{ // test for turbo frame
+            await page.click(`#issues-tab`);
             await page.waitForSelector(s, { timeout: 5000 });
             console.info('✅', s);
         }catch(e){
